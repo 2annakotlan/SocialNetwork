1612b4e0ffa3ff2ea9f07c17bec755f6c1c74f57
Anna Kotlan <2annakotlan@gmail.com>
2024-11-18 16:04:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
-st.write("NO")
-# Use Streamlit's text_input for user input
+
 name = st.text_input("Please enter your name:")
 
-# Display the user's input if provided
 if name:
   st.write(f"Your name is: {name}")

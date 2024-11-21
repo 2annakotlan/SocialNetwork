3bac868100318afc0149a2eda1ce3a935e37e3b4
Anna Kotlan <2annakotlan@gmail.com>
2024-11-21 13:59:29 -0500

    Update googlesheets.py

--- googlesheets.py
+++ googlesheets.py
@@ -4,7 +4,7 @@ from googleapiclient.discovery import build
 from google.oauth2.service_account import Credentials
 
 def get_googlesheets_api():
-    service_account_credentials = st.secrets["google_service_account"] 
+    service_account_credentials = st.secrets["google_service_account"]
     api_scopes = ['https://www.googleapis.com/auth/spreadsheets']  
     google_credentials = Credentials.from_service_account_info(service_account_credentials, scopes=api_scopes)
     sheets_service = build('sheets', 'v4', credentials=google_credentials)
@@ -19,7 +19,7 @@ def get_googlesheets_data(sheets_service):
     data_range = f"Sheet1!A1:{chr(64 + num_columns)}{num_rows}" 
     sheet_data = sheets_service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=data_range).execute()
     sheet_rows = sheet_data.get('values', [])  
-    return pd.DataFrame(sheet_rows[1:], columns=sheet_rows[0])  
+    return pd.DataFrame(sheet_rows[1:], columns=sheet_rows[0]) 
 
-
-
+sheets_service = get_googlesheets_api()
+df = get_googlesheets_data(sheets_service)

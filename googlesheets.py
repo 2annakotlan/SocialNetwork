a162c59c7e90adac245a0133664ba32d917cd7d4
Anna Kotlan <2annakotlan@gmail.com>
2024-11-21 13:20:05 -0500

    Update and rename google_sheets.py to googlesheets.py

--- googlesheets.py
+++ googlesheets.py
@@ -2,14 +2,14 @@ import pandas as pd
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 
-def get_api():
+def get_googlesheets_api():
     service_account_credentials = st.secrets["google_service_account"]  # Load service account credentials
     api_scopes = ['https://www.googleapis.com/auth/spreadsheets']  # Define required scope
     google_credentials = Credentials.from_service_account_info(service_account_credentials, scopes=api_scopes)
     sheets_service = build('sheets', 'v4', credentials=google_credentials)
     return sheets_service
 
-def get_sheets_data(sheets_service):
+def get_googlesheets_data(sheets_service):
     sheet_info = sheets_service.spreadsheets().get(spreadsheetId='1g_upGl2tligN2G7OjVDDIIjVXuhFCupkJME4vPDL7ro').execute()
     sheet_properties = sheet_info['sheets'][0]['properties']['gridProperties']
     num_rows, num_columns = sheet_properties['values']  # Get last row and column

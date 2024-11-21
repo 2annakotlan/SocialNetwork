05708b76ba7d5b34ac754dc3f343e626f6fea03a
Anna Kotlan <2annakotlan@gmail.com>
2024-11-21 12:46:09 -0500

    Delete data_operations.py

--- data_operations.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def append_name_to_dataframe(df, new_name):
-    if new_name and new_name not in df['name'].values:
-        new_row = pd.DataFrame([{'name': new_name}])  
-        df = pd.concat([df, new_row], ignore_index=True)  
-    return df

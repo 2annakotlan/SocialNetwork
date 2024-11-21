be95243517ef0f37dfe17788446e269828571db1
Anna Kotlan <2annakotlan@gmail.com>
2024-11-21 13:21:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,6 @@
 import pandas as pd
 import streamlit as st
-from googleapiclient.discovery import build
-from google.oauth2.service_account import Credentials
-
-# Google Sheets API setup
-service = build('sheets', 'v4', credentials=Credentials.from_service_account_info(st.secrets["google_service_account"], scopes=['https://www.googleapis.com/auth/spreadsheets']))  # Authenticate and build client
-SPREADSHEET_ID = '1g_upGl2tligN2G7OjVDDIIjVXuhFCupkJME4vPDL7ro'
-
-# Fetch data from Google Sheets
-sheet = service.spreadsheets().get(spreadsheetId=SPREADSHEET_ID).execute()['sheets'][0]
-last_row, last_column = sheet['properties']['gridProperties'].values()
-RANGE_NAME = f"Sheet1!A1:{chr(64 + last_column)}{last_row}"
-rows = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute().get('values', [])
-
-# Create DataFrame
-df = pd.DataFrame(rows[1:], columns=rows[0])  # Use first row as headers
+from googlesheets.py import df
 
 # Display data in Streamlit
 st.write("Here is the data:")

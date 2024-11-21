8bd882e4b719a4dbce36bffee79c31c4e0caa07f
Anna Kotlan <2annakotlan@gmail.com>
2024-11-21 12:46:19 -0500

    Delete streamlit_interface.py

--- streamlit_interface.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def display_dataframe(df):
-    st.write("Here is the data:")
-    st.dataframe(df)
-
-def get_user_input():
-    return st.text_input("Enter a name to add:")

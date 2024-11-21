c3020cf8a42ce0f86f10904af419c7d04c102338
Anna Kotlan <2annakotlan@gmail.com>
2024-11-21 13:43:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-import time
 import pandas as pd
 import streamlit as st
-from googlesheets import get_googlesheets_api, get_googlesheets_data
+from googlesheets import df
 
-while True:
-    sheets_service = get_googlesheets_api()
-    df = get_googlesheets_data(sheets_service)
-    st.write(df)
-    time.sleep(10)
+
+st.write(df)
+

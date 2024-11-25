1ed47c15b510af1f237bd1ca4baf7d93132a67d5
Anna Kotlan <2annakotlan@gmail.com>
2024-11-25 16:37:12 -0500

    Update pages.py

--- pages.py
+++ pages.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-def display_landing_page():
+def landing_page():
     st.write("Landing Page")
     if st.button('Student Sign In'):
         st.session_state.page = "create_account_page"
 
-def display_create_account_page():
+def create_account_page():
     st.write("Create Account")

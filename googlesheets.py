8f735af97c3e94115f38e3a70c146d784fd25182
Anna Kotlan <2annakotlan@gmail.com>
2024-11-25 12:07:36 -0500

    Update googlesheets.py

--- googlesheets.py
+++ googlesheets.py
@@ -15,16 +15,10 @@ def get_sheet_data(service):
 def append_row_to_sheet(service, name, friends, interests, activities):
     friends = friends if friends else ""
     activities = activities if activities else ""
-    
-    if not name or not interests:
-        st.error("Name and Interests are required fields.")
-        return None
-    
     new_row = [name, friends, interests, activities]
     service.spreadsheets().values().append(spreadsheetId='1g_upGl2tligN2G7OjVDDIIjVXuhFCupkJME4vPDL7ro', range="Sheet1", valueInputOption="RAW", body={'values': [new_row]}).execute()
     st.success("Info added!")
     return get_sheet_data(service)
-
 
 def delete_row_by_name(service, name):
     df = get_sheet_data(service)

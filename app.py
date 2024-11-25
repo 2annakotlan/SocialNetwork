f013accfb311461f9a7ad133753341a41f60fb9d
Anna Kotlan <2annakotlan@gmail.com>
2024-11-25 16:26:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from landing_page import get_landing_page
+from landing_page import display_landing_page
 import streamlit as st
 
-if st.session_state.page == 'create_account_page':
-    get_create_account_page()  
+display_landing_page()

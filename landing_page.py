df1989e7708ff7c920e7beb7e21d3aa782f68f9c
Anna Kotlan <2annakotlan@gmail.com>
2024-11-25 16:31:05 -0500

    Update landing_page.py

--- landing_page.py
+++ landing_page.py
@@ -4,3 +4,5 @@ def display_landing_page():
   if st.button('Student Sign In'):
       st.session_state.page = "create_account_page"
 
+def display_create_account_page():
+  st.write("Create Account")

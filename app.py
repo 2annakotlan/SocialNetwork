66524c89af022c89d6391e7a1e0a29611831f8a3
Anna Kotlan <2annakotlan@gmail.com>
2024-11-25 15:50:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,15 @@ with st.form(key="input_form"):
     friends = st.text_area("Friends (comma separated):", value="")
     interests = st.text_area("Interests (comma separated):")
     activities = st.text_area("Activities (comma separated):", value="")
-    add = st.form_submit_button("Add Entry")
-    delete = st.form_submit_button("Delete Entry")
-    edit = st.form_submit_button("Edit Entry")
+    add = st.form_submit_button("Create Account")
+    delete = st.form_submit_button("Delete Account")
+    edit = st.form_submit_button("Edit Account Information")
 
 if add:
     df = create_account(get_sheets_service(), name, friends, interests, activities)
 
 if delete:
     df = delete_account(get_sheets_service(), name)
+
+if edit: 
+    df = edit_account(get_sheets_service(), name, new_friends, new_interests, new_activities)

a3ed025872e1856ac7bbd594532e2faa4616e2f2
Anna Kotlan <2annakotlan@gmail.com>
2024-11-25 16:44:59 -0500

    Update pages.py

--- pages.py
+++ pages.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
 def display_landing_page():
-    st.write("Landing Page")
-    if st.button('Student Sign In'):
-        st.session_state.page = "create_account_page"
-
-def display_student_portal_page():
-    st.write("Create Account")
+    st.write("Sign In")
+    email = st.text_input("Enter your email:", "")

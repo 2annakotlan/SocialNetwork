c3fafd11675b9d7d0859349d83d3ead4f62fb129
Anna Kotlan <2annakotlan@gmail.com>
2024-11-25 16:33:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from landing_page import display_landing_page, display_create_account_page
 import streamlit as st
+from landing_page import display_landing_page, display_create_account_page
 
-display_landing_page()
+# Initialize session state if it's not already set
+if 'page' not in st.session_state:
+    st.session_state.page = 'landing_page'
+
+# Display the appropriate page based on session state
+if st.session_state.page == 'landing_page':
+    display_landing_page()
 
 if st.session_state.page == 'create_account_page':
-    display_create_account_page()  
-  
-
+    display_create_account_page()

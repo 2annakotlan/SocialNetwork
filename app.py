5f25f617258b7b83524453526e7eb187bf87ef4e
Anna Kotlan <2annakotlan@gmail.com>
2024-11-25 16:46:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from pages import display_landing_page, display_student_portal_page
+from pages import display_landing_page
 
 if 'page' not in st.session_state:
     st.session_state.page = 'landing_page'

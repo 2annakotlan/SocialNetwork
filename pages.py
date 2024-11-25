b50ff8837a222416b26fcfab564ecf574754265a
Anna Kotlan <2annakotlan@gmail.com>
2024-11-25 16:38:29 -0500

    Update pages.py

--- pages.py
+++ pages.py
@@ -5,5 +5,5 @@ def landing_page():
     if st.button('Student Sign In'):
         st.session_state.page = "create_account_page"
 
-def create_account_page():
+def student_portal_page():
     st.write("Create Account")

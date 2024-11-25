8ce3da2af4f9dd1845ef234114b19aa7399e655a
Anna Kotlan <2annakotlan@gmail.com>
2024-11-25 16:45:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,3 @@ from pages import display_landing_page, display_student_portal_page
 
 if 'page' not in st.session_state:
     st.session_state.page = 'landing_page'
-
-if st.session_state.page == 'landing_page':
-    display_landing_page()
-
-if st.session_state.page == 'student_portal_page':
-    display_student_portal_page()

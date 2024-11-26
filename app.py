ae0412493254d538e64e12b72922b594624ad18d
Anna Kotlan <2annakotlan@gmail.com>
2024-11-26 14:23:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 from pages import display_landing_page
 
+# Check if 'page' is in session_state, otherwise set default value
 if 'page' not in st.session_state:
-   st.session_state.page = 'landing_page'
+    st.session_state.page = 'landing_page'
 
-
+# Assuming you want to display the landing page:
+if st.session_state.page == 'landing_page':
+    display_landing_page()

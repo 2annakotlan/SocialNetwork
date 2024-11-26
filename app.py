f32e7b34a6b327572e64822a834395e046036ea5
Anna Kotlan <2annakotlan@gmail.com>
2024-11-26 14:22:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
-#from pages import display_landing_page
+from pages import display_landing_page
 
-#if 'page' not in st.session_state:
-   # st.session_state.page = 'landing_page'
+if 'page' not in st.session_state:
+   st.session_state.page = 'landing_page'
 
-st.write("HI")
- 
+

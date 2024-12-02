7bd9f1175f0192132f4aab80c235f9d3b774912b
Anna Kotlan <2annakotlan@gmail.com>
2024-12-02 16:29:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pages import *
 
 if 'page' not in st.session_state:
     st.session_state.page = 'landing_page'
+    
 if st.session_state.page == 'landing_page':
     display_landing_page()
 if st.session_state.page == 'profile_info':

7118eb98c4134c9f3685c0e314401696144902ce
Anna Kotlan <2annakotlan@gmail.com>
2024-12-02 15:54:01 -0500

    Update pages.py

--- pages.py
+++ pages.py
@@ -1,13 +1,18 @@
 import streamlit as st
 
 def display_landing_page():
+    st.write("Landing Page")
     if st.button("Student Login"):
         st.session_state.page = 'student_login_page' 
     if st.button("Admin Login"):
         st.session_state.page = 'admin_login_page' 
  
 def display_student_login_page():
-    st.write("Student Info")
+    st.write("Student Login Page")
+    if st.button("Back"):
+        st.session_state.page = 'landing_page' 
     
 def display_admin_login_page():
-    st.write("Admin Info") 
+    st.write("Admin Login Page")
+    if st.button("Back"):
+        st.session_state.page = 'landing_page' 

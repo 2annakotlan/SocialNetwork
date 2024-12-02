87e53db25840adad3c56df1ab7ba06a390f9f537
Anna Kotlan <2annakotlan@gmail.com>
2024-12-02 16:45:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,20 +13,20 @@ def display_student_login_page():
     st.write("Student Login Page")
     email = st.text_input("Enter your email:")
     if email:
-        display_button("Next", "student_profile_page")
+        display_button("Login", "student_profile_page")
     display_button("Back", "landing_page")
 
 def display_admin_login_page():
     st.write("Admin Login Page")
     email = st.text_input("Enter your email:")
     if email:
-        display_button("Next", "admin_landing_page")
+        display_button("Login", "admin_landing_page")
     display_button("Back", "landing_page")
 
 def display_student_profile_page():
     st.write("Student Profile Page")
-    display_button("Next", "student_landing_page")
-    display_button("Back", "student_login_page")
+    display_button("Save Changes", "student_landing_page")
+    display_button("Log out", "landing_page")
 
 def display_admin_landing_page():
     st.write("Admin Landing Page")

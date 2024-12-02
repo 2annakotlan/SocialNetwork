663ce3290b610c453dd4b3c77e854f42f911b0dc
Anna Kotlan <2annakotlan@gmail.com>
2024-12-02 16:30:37 -0500

    Update pages.py

--- pages.py
+++ pages.py
@@ -4,10 +4,6 @@ def display_button(label, target_page):
     if st.button(label):
         st.session_state.page = target_page
 
-def display_email_button():
-    if st.button("Enter Email"):
-        st.session_state.show_email_input = True
-
 def display_landing_page():
     st.write("Landing Page")
     display_button("Student Login", "student_login_page")
@@ -15,7 +11,6 @@ def display_landing_page():
  
 def display_student_login_page():
     st.write("Student Login Page")
-    email = st.text_input("Enter your email:")
     display_button("Next", "student_profile_page")
     display_button("Back", "landing_page")
     

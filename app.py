dfb14e3891bd7420ac6279bca14325b7ad892c92
Anna Kotlan <2annakotlan@gmail.com>
2024-12-02 16:35:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,52 @@
 import streamlit as st
-from pages import *
+
+def display_button(label, target_page):
+    if st.button(label):
+        st.session_state.page = target_page
+
+def display_landing_page():
+    st.write("Landing Page")
+    display_button("Student Login", "student_login_page")
+    display_button("Admin Login", "admin_login_page")
+
+def display_student_login_page():
+    st.write("Student Login Page")
+    display_button("Next", "student_profile_page")
+    display_button("Back", "landing_page")
+
+def display_admin_login_page():
+    st.write("Admin Login Page")
+    display_button("Next", "admin_landing_page")
+    display_button("Back", "landing_page")
+
+def display_student_profile_page():
+    st.write("Student Profile Page")
+    display_button("Next", "student_landing_page")
+    display_button("Back", "student_login_page")
+
+def display_admin_landing_page():
+    st.write("Admin Landing Page")
+    display_button("Log out", "landing_page")
+
+def display_student_landing_page():
+    st.write("Student Landing Page")
+    display_button("Edit Profile", "student_profile_page")
+    display_button("Log out", "landing_page")
+
 
 if 'page' not in st.session_state:
     st.session_state.page = 'landing_page'
-    
-if st.session_state.page == 'landing_page':
-    display_landing_page()
-if st.session_state.page == 'profile_info':
-    display_student_login_page()
-if st.session_state.page == 'admin_login_page':
-    display_admin_login_page()
-if st.session_state.page == 'student_login_page':
-    display_student_login_page()
-if st.session_state.page == 'student_profile_page':
-    display_student_profile_page()
-if st.session_state.page == 'student_landing_page':
-    display_student_landing_page()
-if st.session_state.page == 'admin_landing_page':
-    display_admin_landing_page()
+
+page_functions = {
+    'landing_page': display_landing_page,
+    'student_login_page': display_student_login_page,
+    'admin_login_page': display_admin_login_page,
+    'student_profile_page': display_student_profile_page,
+    'admin_landing_page': display_admin_landing_page,
+    'student_landing_page': display_student_landing_page
+}
+
+if st.session_state.page in page_functions:
+    page_functions[st.session_state.page]()
+else:
+    st.error("Page not found!")

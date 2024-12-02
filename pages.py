a2adcbd2619efd26d0d700e453223ca030e0d485
Anna Kotlan <2annakotlan@gmail.com>
2024-12-02 16:25:54 -0500

    Update pages.py

--- pages.py
+++ pages.py
@@ -21,7 +21,18 @@ def display_student_login_page():
     
 def display_admin_login_page():
     st.write("Admin Login Page")
+    display_button("Next", "admin_landing_page")
     display_button("Back", "landing_page")
 
 def display_student_profile_page():
     st.write("Student Profile Page")
+    display_button("Next", "student_landing_page")
+
+def display_admin_landing_page():
+    st.write("Admin Landing Page")
+    display_button("Log out", "landing_page")
+
+def display_student_landing_page():
+    st.write("Student Landing Page")
+    display_button("Edit Profile", "student_profile_page")
+    display_button("Log out", "landing_page")

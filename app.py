abf42a003421c32f57ac4539487f0c6ea7ef5772
Anna Kotlan <2annakotlan@gmail.com>
2024-12-02 16:28:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,7 @@ if st.session_state.page == 'student_login_page':
     display_student_login_page()
 if st.session_state.page == 'student_profile_page':
     display_student_profile_page()
+if st.session_state.page == 'student_landing_page':
+    display_student_landing_page()
+if st.session_state.page == 'admin_landing_page':
+    display_admin_landing_page()

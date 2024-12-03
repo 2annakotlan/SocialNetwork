0a23adc56ed230cf361bc74776dec9aa20e78e51
Anna Kotlan <2annakotlan@gmail.com>
2024-12-02 23:15:25 -0500

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -1,29 +1,25 @@
 import streamlit as st
+import time
 
 def display_button(label, target_page):
     if st.button(label):
         st.session_state.page = target_page
-        
-def display_email_buttons(login_target_page, signup_target_page):
-    #email = st.text_input("Email:")
-    email = st.text_input("Email:", value="admin:")
-    existing_domain = "@gmail.com"
-    non_existing_domain = not existing_domain
-    
-    
-    if st.button("Login"):
-        if email == f"admin{existing_domain}":
-            st.session_state.page = login_target_page
-        elif not email.startswith(f"admin"):
-            st.error("Please use an admin email")
-        elif not email.endswith(existing_domain): 
-            st.error("Please use an existing domain")
 
-    if st.button("Sign Up"):
-        if email == f"admin{non_existing_domain}":
+def display_email_buttons(login_target_page, signup_target_page):
+    email = st.text_input("Email:")
+    existing_domain = "@gmail.com"
+    
+    if st.button("Next"):
+        if email == f"admin{existing_domain}":
+            st.message("Logging In")
+            time.sleep(3)  
+            st.session_state.page = login_target_page
+        elif email.startswith("admin") and not email.endswith(existing_domain):
             st.session_state.page = signup_target_page
-        elif not email.startswith(f"admin"):
+            st.message("Signing Up")
+            time.sleep(3)  
+        elif not email.startswith("admin"):
             st.error("Please use an admin email")
-        elif not email.endswith(non_existing_domain): 
-            st.error("Domain already exists")
+
+
 

03d941c19439b3ae4879a679c8ad4adb9cb7ff37
Anna Kotlan <2annakotlan@gmail.com>
2024-12-02 23:51:43 -0500

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -15,12 +15,12 @@ def display_student_email_button(login_target_page, signup_target_page):
             st.session_state.page = login_target_page
             st.success("Logging In")
             time.sleep(3)  
-        elif not email.startswith("existing_username") and not email.startswith("admin") and email.endswith(existing_domain):
+        elif email.startswith("admin"):
+            st.error("Looks like you are trying to sign in as an administrator.")
+        elif not email.startswith("existing_username") and email.endswith(existing_domain):
             st.session_state.page = login_target_page
             st.success("Signing Up")
             time.sleep(3)  
-        elif email.startswith("admin"):
-            st.error("Looks like you are trying to sign in as an administrator.")
         elif not email.endswith(existing_domain):
             st.error("Your organization has not created an account with us.")
 

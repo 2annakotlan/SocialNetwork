3a467ef759025c11f47cf72683f5137297c1a61e
Anna Kotlan <2annakotlan@gmail.com>
2024-12-02 23:17:15 -0500

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -11,12 +11,12 @@ def display_email_buttons(login_target_page, signup_target_page):
     
     if st.button("Next"):
         if email == f"admin{existing_domain}":
-            st.message("Logging In")
+            st.success("Logging In")
             time.sleep(3)  
             st.session_state.page = login_target_page
         elif email.startswith("admin") and not email.endswith(existing_domain):
             st.session_state.page = signup_target_page
-            st.message("Signing Up")
+            st.success("Signing Up")
             time.sleep(3)  
         elif not email.startswith("admin"):
             st.error("Please use an admin email")

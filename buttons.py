ff74b9c5011e21b4ee021b01f8ac336ba24c3044
Anna Kotlan <2annakotlan@gmail.com>
2024-12-02 23:02:19 -0500

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -10,7 +10,7 @@ def display_email_buttons(login_target_page, signup_target_page):
     non_existing_domain = "@yahoo.com"
     
     if st.button("Login"):
-        if email == f"admin{existing_domain}"
+        if email == f"admin{existing_domain}":
             st.session_state.page = login_target_page
         elif not email.startswith(f"admin"):
             st.error("Please use an admin email")
@@ -18,7 +18,7 @@ def display_email_buttons(login_target_page, signup_target_page):
             st.error("Please use an existing domain")
 
     if st.button("Sign Up"):
-        if email == f"admin{non_existing_domain}"
+        if email == f"admin{non_existing_domain}":
             st.session_state.page = signup_target_page
         elif not email.startswith(f"admin"):
             st.error("Please use an admin email")

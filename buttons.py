83962d34a78e0b7d3c8e88db980ddb19ed5f71cb
Anna Kotlan <2annakotlan@gmail.com>
2024-12-02 23:39:19 -0500

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -5,12 +5,29 @@ def display_button(label, target_page):
     if st.button(label):
         st.session_state.page = target_page
 
+def display_student_email_button(login_target_page, signup_target_page):
+    email = st.text_input("Email:")
+    existing_domain = "@gmail.com" 
+    existing_username = "akotlan"
+    
+    if st.button("Enter"):
+        if email.startswith("existing_username") and email.endswith(existing_domain):
+            st.session_state.page = login_target_page
+            st.success("Logging In")
+            time.sleep(3)  
+        elif not email.startswith("existing_username") and email.endswith(existing_domain):
+            st.session_state.page = login_target_page
+            st.success("Signing Up")
+            time.sleep(3)  
+        elif not email.endswith(existing_domain):
+            st.error("Your organization has not created an account with us.")
+
 def display_admin_email_button(login_target_page, signup_target_page):
     email = st.text_input("Email:")
     existing_domain = "@gmail.com" 
     
     if st.button("Next"):
-        if email == f"admin{existing_domain}":
+        if email.startswith("admin") and email.endswith(existing_domain):
             st.session_state.page = login_target_page
             st.success("Logging In")
             time.sleep(3)  
@@ -21,21 +38,6 @@ def display_admin_email_button(login_target_page, signup_target_page):
         elif not email.startswith("admin"):
             st.error("Please use an admin email")
 
-def display_student_email_button(login_target_page, signup_target_page):
-    email = st.text_input("Email:")
-    existing_domain = "@gmail.com" 
-    
-    if st.button("Next"):
-        if email.startswith("admin"):
-            st.error("Please do not use an admin email")
-        elif email.endswith(existing_domain):
-            st.session_state.page = login_target_page
-            st.success("Logging In")
-            time.sleep(3)  
-        elif not email.endswith(existing_domain):
-            st.session_state.page = signup_target_page
-            st.success("Signing Up")
-            time.sleep(3)  
 
 
 

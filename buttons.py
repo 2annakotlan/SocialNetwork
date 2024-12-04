cf7b696ba55fbe2a570d2e7d47cef340f2b9831c
Anna Kotlan <2annakotlan@gmail.com>
2024-12-04 18:41:44 -0500

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -22,6 +22,7 @@ def display_student_email_button(login_target_page, signup_target_page):
         elif not email.endswith(existing_domain): 
             st.error("Your organization has not created an account with us.")
 
+'''
 def display_admin_email_button(login_target_page, signup_target_page):
     email = st.text_input("Email:")
     existing_domain = "@gmail.com" 
@@ -37,4 +38,26 @@ def display_admin_email_button(login_target_page, signup_target_page):
             time.sleep(3)  
         elif not email.startswith("admin"):
             st.error("Please use an admin email")
+'''
+
+from google_sheets import *
+
+def display_admin_email_button(login_target_page, signup_target_page):
+    email = st.text_input("Email:")
+
+    service = get_sheets_service()
+    institutions_df = get_sheet_data(service, "Institutions")  # Get the entire DataFrame
+    existing_domains = institutions_df['institution'].tolist()  # Extract 'institution' column as a list of domains
+
+    if st.button("Enter"):
+        if email.startswith("admin") and any(email.endswith(domain) for domain in existing_domains):
+            st.session_state.page = login_target_page
+            st.success("Logging In")
+            time.sleep(3)  
+        elif email.startswith("admin"):
+            st.session_state.page = signup_target_page
+            st.success("Signing Up")
+            time.sleep(3)  
+        else:
+            st.error("Please use an admin email with a valid institution domain")
 

569dae5b0bf0d48478f3046f1033b6d7d86d0cc1
Anna Kotlan <2annakotlan@gmail.com>
2024-12-15 02:42:22 -0500

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -22,6 +22,13 @@ def create_new_sheet(new_sheet_name):
 
 def append_values(input_data, sheet_name, column_name):
     headers = service.spreadsheets().values().get(spreadsheetId='1g_upGl2tligN2G7OjVDDIIjVXuhFCupkJME4vPDL7ro', range=f'{sheet_name}!1:1').execute().get('values', [])[0]
+    col = chr(65 + headers.index(column_name) + 1) 
+    st.write(col)
+    service.spreadsheets().values().append(spreadsheetId='1g_upGl2tligN2G7OjVDDIIjVXuhFCupkJME4vPDL7ro', range=f'{sheet_name}!{col}:{col}', valueInputOption="RAW", body={'values': [[input_data]]}).execute()
+
+'''
+def append_values(input_data, sheet_name, column_name):
+    headers = service.spreadsheets().values().get(spreadsheetId='1g_upGl2tligN2G7OjVDDIIjVXuhFCupkJME4vPDL7ro', range=f'{sheet_name}!1:1').execute().get('values', [])[0]
     col_letter = chr(65 + headers.index(column_name))  # Convert column index to letter
     st.write(col_letter)
     service.spreadsheets().values().append(
@@ -31,7 +38,6 @@ def append_values(input_data, sheet_name, column_name):
         body={'values': [[input_data]]}
     ).execute()
 
-'''
 def append_values(input_data, sheet_name, column_name):
     headers = service.spreadsheets().values().get(spreadsheetId='1g_upGl2tligN2G7OjVDDIIjVXuhFCupkJME4vPDL7ro', range=f'{sheet_name}!1:1').execute().get('values', [])[0]
     col = chr(65 + headers.index(column_name))

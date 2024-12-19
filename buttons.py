b53e9eb8c5795a4c74a06c33713f6d1ebf867a4b
Anna Kotlan <2annakotlan@gmail.com>
2024-12-19 18:56:19 -0500

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -34,8 +34,8 @@ def display_admin_email_button(login_target_page, signup_target_page):
             st.success("Logging In")
             time.sleep(3)  
         else: # not existing email --> signing up
-            append_cell("admin", "email", email)
-            #edit_cell("admin", "domain", "admin@harvard.edu", "harvard.edu")
+            edit_cell("admin", "email", "append", email)
+            edit_cell("admin", "domain", email, "test.edu")
             st.session_state.page = signup_target_page  
             st.success("Signing Up")
             time.sleep(3)  

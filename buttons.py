39d72122cc6a58198f65bdf7a42d6c990fe5f78d
Anna Kotlan <2annakotlan@gmail.com>
2024-12-19 16:28:40 -0500

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -35,9 +35,9 @@ def display_admin_email_button(login_target_page, signup_target_page):
             st.success("Logging In")
             time.sleep(3)  
         else: # not existing email --> signing up
-            append_cell("Admin", "email", email)
+            @append_cell("Admin", "domain", email)
 
-            # edit_cell("Admin", "C4", "New Value")
+            edit_cell("Admin", "C4", "New Value")
             #edit_cell(sheet_name="Admin", column_name="email", row_name="append", new_value=email)
             #edit_cell("Admin", "email", "append", email)
             #edit_cell("Admin", "domain", email, domain)

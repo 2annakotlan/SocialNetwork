64a967ad911e44cb342cdb7b0df00dc647a993c4
Anna Kotlan <2annakotlan@gmail.com>
2024-12-20 20:50:42 -0500

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -46,10 +46,6 @@ def display_admin_email_button(login_target_page, signup_target_page):
             st.success("Signing Up")
             time.sleep(3)  
 
-#def display_admin_profile_button():
-    #existing_headers = get_headers(domain)
-    #st.write(existing_headers)
-
 
 
                           

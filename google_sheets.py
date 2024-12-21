adf1d9c6a4c0da281f181bc543ea0800135a5856
Anna Kotlan <2annakotlan@gmail.com>
2024-12-20 20:03:32 -0500

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -11,11 +11,24 @@ def get_sheets_service():
 
 service = get_sheets_service()
 
-def get_sheet_column_data(sheet_name, column_name):
+def get_column_data(sheet_name, column_name):
     result = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=sheet_name).execute()
     data = result.get('values', []) 
     df = pd.DataFrame(data[1:], columns=data[0])
-    return df[[column_name]]
+    column_data = df[[column_name]] 
+    return column_data if not column_data.empty else None
+
+def get_data(sheet_name, row_name, column_name):
+    result = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=sheet_name).execute()
+    data = result.get('values', [])
+    df = pd.DataFrame(data[1:], columns=data[0])
+    if row_name is None and column_name is not None:
+        data = df[[column_name]]  
+    elif column_name is None and row_name is not None:
+        data = df[df.iloc[:, 0] == row_name] 
+    elif row_name is not None and column_name is not None:
+        data = df[df.iloc[:, 0] == row_name][[column_name]]  # Filter by row and column
+    return data if not data.empty else None
 
 def create_new_sheet(new_sheet_name):
     service.spreadsheets().batchUpdate(spreadsheetId=spreadsheetId, body={'requests': [{"addSheet": {"properties": {"title": new_sheet_name}}}]}).execute()

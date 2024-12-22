4ff10e208c5f0d39af72882d65d882a38bfc76c7
Anna Kotlan <2annakotlan@gmail.com>
2024-12-22 17:47:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,15 +11,24 @@ def display_student_start_page():
     display_student_email_button("student_landing_page", "student_profile_page")
     display_button("Back", "landing_page")
 
+def display_student_profile_page():
+    st.write("Student Profile Page")
+    display_button("Save Changes", "student_landing_page")
+    display_button("Log out", "landing_page")
+
+def display_student_landing_page():
+    st.write("Student Landing Page")
+    display_button("Edit Profile", "student_profile_page")
+    display_button("Log out", "landing_page")
+
 def display_admin_start_page():
     st.write("Admin Start Page")
     display_admin_email_button("admin_landing_page", "admin_profile_page")
     display_button("Back", "landing_page")
 
-def display_student_profile_page():
-    st.write("Student Profile Page")
-    display_button("Save Changes", "student_landing_page")
-    display_button("Log out", "landing_page")
+def display_admin_institution_page():
+    st.write("Admin Institution Page")
+    display_admin_institution_button()
 
 def display_admin_profile_page():
     st.write("Admin Profile Page")
@@ -30,11 +39,6 @@ def display_admin_profile_page():
 def display_admin_landing_page():
     st.write("Admin Landing Page")
     display_button("Edit Profile", "admin_profile_page")
-    display_button("Log out", "landing_page")
-
-def display_student_landing_page():
-    st.write("Student Landing Page")
-    display_button("Edit Profile", "student_profile_page")
     display_button("Log out", "landing_page")
 
 if 'page' not in st.session_state:

b5352eaa9808a89c2d7b8c3d427d106a7a3d3e68
Anna Kotlan <2annakotlan@gmail.com>
2024-12-29 23:54:43 -0500

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -27,8 +27,8 @@ def display_admin_email_button(login_target_page, signup_target_page):
     existing_emails = get_data("admin", None, "email")
     st.write(existing_emails)
     email = st.text_input("Email:")
-    institution = st.text_input("Institution:", value="Enter your institution name here")
-    #institution = st.text_input("Institution:") if email not in existing_emails.values and email != "" else None
+    institution_guess = email.split('@')[1].split('.edu')[0].capitalize()
+    institution = st.text_input("Institution:", value=institution_guess) if email not in existing_emails.values and email != "" else None
     
     if st.button("Enter"):   
         st.session_state.admin_email = email    

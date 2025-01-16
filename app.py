eb6e85f224f629007cee984c21c9c825d45fece7
Anna Kotlan <2annakotlan@gmail.com>
2025-01-16 13:51:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -28,12 +28,12 @@ def display_admin_start_page():
     display_button("Back", "landing_page")
 
 def display_admin_signin_page():
-    st.write("Admin Signin Page")
+    st.write("Admin Sign In Page")
     display_button("Sign In", "admin_landing_page")
     display_button("Back", "admin_start_page")
 
 def display_admin_signup_page():
-    st.write("Admin Signup Page")
+    st.write("Admin Sign Up Page")
     display_button("Sign Up", "admin_profile_page")
     display_button("Back", "admin_start_page")
 

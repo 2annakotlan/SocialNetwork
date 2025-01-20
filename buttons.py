8cac7810beceb6f69c38bfc3731fd661f458d3f7
Anna Kotlan <2annakotlan@gmail.com>
2025-01-20 13:45:57 -0500

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -9,11 +9,12 @@ def display_button(label, target_page):
 def display_student_email_button(label, signin_target_page, signup_target_page):
     email = st.text_input("Email:")
     existing_admin_emails = read_sheet()
-    st.write(existing_admin_emails)
     if st.button(label):
         admin_email = f"admin@{email.split('@')[1]}"
         if admin_email in existing_admin_emails: 
             existing_student_emails = read_values(sheet_name=admin_email, column_name="email", row_name=None)
+            st.write(existing_student_emails)
+            '''
             if email in existing_student_emails.values: # <-- SIGNING IN
                 st.success("Signing In")
                 st.session_state.email = email
@@ -23,8 +24,10 @@ def display_student_email_button(label, signin_target_page, signup_target_page):
                 st.session_state.email = email
                 st.session_state.page = signup_target_page
                 edit_values(sheet_name=admin_email, column_name="email", row_name="append", value=email)
+            '''
         if not admin_email in existing_admin_emails: # <-- INSTITUTION HAS NOT MADE AN ACCOUNT
             st.error("Your Institution Has Not Made an Account with Us")
+            
 
 
 def display_admin_email_button(label, signin_target_page, signup_target_page):
